b370b17e158e992f3ef85aef34ed8ad4893ffc9b
udueirgvj <lllsgl8j@gmail.com>
2026-04-10 09:50:42 +0300

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:termeen/main.dart';
+
+void main() {
+  testWidgets('App smoke test', (WidgetTester tester) async {
+    // Basic smoke test
+    expect(true, isTrue);
+  });
+}
